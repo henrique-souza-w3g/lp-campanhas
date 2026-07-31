832a110d5c558d73a9cec57f5b94a53d9d844ed0
henrique-souza-w3g <henrique.souza@w3gconsultoria.com.br>
2026-07-31 12:56:55 -0300

    componentizando Welcome

--- lp-agosto-lilas/src/app.tsx
+++ lp-agosto-lilas/src/app.tsx
@@ -1,15 +1,13 @@
 import "./app.css";
 import { Header } from "./components/Header";
+import Welcome from "./components/Welcome";
 
 export function App() {
   return (
     <>
       <Header />
       <main className="container">
-        <section className="welcome">
-          <h1 className="welcome-title">AGOSTO</h1>
-          <h2 className="welcome-subtitle">Lilás</h2>
-        </section>
+        <Welcome />
       </main>
     </>
   );
---
+++ lp-agosto-lilas/src/components/Welcome.tsx
@@ -0,0 +1,10 @@
+export default function Welcome() {
+  return (
+    <section className="welcome">
+      <div className="welcome-heading stack">
+        <h1 className="welcome-title">AGOSTO</h1>
+        <h2 className="welcome-subtitle">Lilás</h2>
+      </div>
+    </section>
+  );
+}
