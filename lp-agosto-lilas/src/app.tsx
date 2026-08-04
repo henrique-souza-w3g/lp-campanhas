116c672648047c671258189ea050dd15f44823e2
henrique-souza-w3g <henrique.souza@w3gconsultoria.com.br>
2026-08-04 10:57:25 -0300

    commiting variables not used

--- lp-agosto-lilas/src/app.tsx
+++ lp-agosto-lilas/src/app.tsx
@@ -2,13 +2,13 @@ import { MotionConfig } from "motion/react";
 import "./app.css";
 import AboutPurple from "./components/purple/AboutPurple";
 import FooterPurple from "./components/purple/FooterPurple";
-import { HeaderPurple } from "./components/purple/HeaderPurple";
+// import { HeaderPurple } from "./components/purple/HeaderPurple";
 import TiposViolenciaPurple from "./components/purple/TiposViolenciaPurple";
 import WelcomePurple from "./components/purple/WelcomePurple";
-import { useState } from "preact/hooks";
+// import { useState } from "preact/hooks";
 
 export function App() {
-  const [monthChoosed, setMonthChoosed] = useState<number>(0);
+  // const [monthChoosed, setMonthChoosed] = useState<number>(0);
 
   return (
     <MotionConfig reducedMotion="user">
